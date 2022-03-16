077718e0da44dadb9ac1aae830437bd8a3115eed
Vikas Rajput <vikassinghrajput551@gmail.com>
2022-03-16 22:45:47 +0530

    Dark Mode
    Only for desktop

---
+++ script.js
@@ -0,0 +1,16 @@
+var swich = document.getElementById('switch');
+var cover = document.getElementById("cover");
+var maincard = document.getElementById("main_container");
+var display = document.getElementById("display");
+var input = document.getElementById("input_container");
+var toggleText = document.getElementById("bottom_container");    
+var logo = document.getElementById("logo_container");
+
+swich.onclick = function() {
+    cover.classList.toggle("cover_dark_theme");
+    maincard.classList.toggle("dark_main_container");
+    display.classList.toggle("display_dark");
+    input.classList.toggle("input_container_dark");
+    toggleText.classList.toggle("bottom_container_dark");
+    logo_container.classList.toggle("logo_container_dark")
+}
